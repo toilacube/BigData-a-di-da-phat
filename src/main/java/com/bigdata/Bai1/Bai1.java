d5f183ce5a99ffa82db0898e7d1d9df26329f9d6
toilacube <151115rubikcube@gmail.com>
2024-09-30 09:49:45 +0700

    Test 1 lan nua

--- src/main/java/com/bigdata/Bai1/Bai1.java
+++ src/main/java/com/bigdata/Bai1/Bai1.java
@@ -1,4 +1,58 @@
 package com.bigdata.Bai1;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class Bai1 {
+    public static class Bai1Mapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String[] tokens = value.toString().split(" ");
+            for (String token : tokens) {
+                word.set(token);
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class Bai1Reducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(Bai1.class);
+        job.setMapperClass(Bai1Mapper.class);
+        job.setCombinerClass(Bai1Reducer.class);
+        job.setReducerClass(Bai1Reducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+      }
 }
