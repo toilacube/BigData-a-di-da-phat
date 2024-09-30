3f54629533584479e4540a91ad54be7439f8329a
toilacube <151115rubikcube@gmail.com>
2024-09-30 11:38:07 +0700

    Done bai 2a

---
+++ src/main/java/com/bigdata/Bai2/Bai2a.java
@@ -0,0 +1,68 @@
+package com.bigdata.Bai2;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Bai2a {
+    public static class MMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+         private Text productID = new Text();
+        private DoubleWritable salesAmount = new DoubleWritable();
+
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String[] fields = value.toString().split(",");
+
+            if (fields.length == 4) {
+                String productId = fields[1].trim();  // Mã sản phẩm
+                double unitPrice = Double.parseDouble(fields[2].trim()); // Đơn giá
+                int quantity = Integer.parseInt(fields[3].trim()); // Số lượng
+
+                double sales = unitPrice * quantity;
+
+                productID.set(productId);
+                salesAmount.set(sales);
+
+                context.write(productID, salesAmount);
+            }
+        }
+    }
+
+    public static class RReducer
+            extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+        private DoubleWritable result = new DoubleWritable();
+
+        public void reduce(Text key, Iterable<DoubleWritable> values,
+                Context context) throws IOException, InterruptedException {
+            double sum = 0;
+            for (DoubleWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Web count");
+        job.setJarByClass(Bai2a.class);
+        job.setMapperClass(MMapper.class);
+        job.setCombinerClass(RReducer.class);
+        job.setReducerClass(RReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ src/main/java/com/bigdata/Bai2/README.md
@@ -0,0 +1,8 @@
+
+Rồi mở terminal chạy
+
+```
+hadoop jar target/BigData-a-di-da-phat-1.0-SNAPSHOT.jar com.bigdata.Bai2.Bai2a /bai2 /bai2_output
+```
+
+Chạy xong dô `http://localhost:9870/explorer.html#/bai2_output` là thấy file output
